be9666472372e55c6609c6fcf020e42d6a43368b
Santiago Rodriguez <srodriguez@redventures.com>
2021-02-10 16:28:11 -0500

    daily sql and codewar

--- week_1/codewars_week1.js
+++ week_1/codewars_week1.js
@@ -26,16 +26,38 @@ spinWords("test Hello there")
 //The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
 //To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
 
-function openOrSenior(data){
+function openOrSenior(data) {
     results = []
     data.forEach(person => {
-        if (person[0]>=55 && person[1]>7){
+        if (person[0] >= 55 && person[1] > 7) {
             results.push('Senior')
         } else {
             results.push('Open')
         }
     })
     return results
-  }
+}
 
-  openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])
+openOrSenior([[45, 12], [55, 21], [19, -2], [104, 20]])
+
+//2.10
+//Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+//Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+function maskify(cc) {
+    result = ""
+    ccr = cc.split("").reverse().join("")
+
+    for (let i = 0; i < ccr.length; i++) {
+        if (i > 3) {
+            result += "#"
+        } else {
+            result += ccr[i]
+        }
+    }
+
+    result = result.split("").reverse().join("")
+    return result
+}
+
+maskify("should")
